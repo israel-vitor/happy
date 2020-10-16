c3c45b8ca5ad20e379d8e2bc98d351cb7ce858a2
Israel Vitor Lima <israel.vitor.dev@gmail.com>
2020-10-16 20:16:22 -0300

    feat: create and style the orphanage map page

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import mapMarker from '../images/mapmarker.svg';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import '../styles/pages/orphanages-map.css';
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'
+
+function OrphanagesMap() {
+    return (
+        <div id="map-page">
+            <aside>
+                <header>
+                    <img src={mapMarker} alt="Happy" />
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando sua visita :)</p>
+
+                </header>
+
+                <footer>
+                    <strong>São Luís</strong>
+                    <span>Maranhão</span>
+                </footer>
+
+            </aside>
+
+            <Map
+                center={[-2.5591798, -44.2007552]}
+                zoom={13}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer
+                    url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
+            </Map>
+
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+
+        </div>
+    );
+}
+
+export default OrphanagesMap;
