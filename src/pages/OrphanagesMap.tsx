88a905fa7c29cad04965c551844cb8147973b542
Israel Vitor Lima <israel.vitor.dev@gmail.com>
2020-11-10 17:36:31 -0300

    feat: add custom marker to orphanages map

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import mapMarker from '../images/mapmarker.svg';
+import Leaflet from 'leaflet';
+
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import '../styles/pages/orphanages-map.css';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+
+import mapMarker from '../images/mapmarker.svg';
+
 import 'leaflet/dist/leaflet.css'
+import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarker,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
 
 function OrphanagesMap() {
     return (
@@ -30,9 +42,17 @@ function OrphanagesMap() {
                 zoom={13}
                 style={{ width: '100%', height: '100%' }}
             >
-                <TileLayer
-                    url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-                />
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+                <Marker position={[-2.5591798, -44.2007552]} icon={mapIcon}>
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Casa dos doces
+                        <Link to="">
+                            <FiArrowRight size={20} color="#FFF"/>
+                        </Link>
+                    </Popup>
+                </Marker>
+
             </Map>
 
 
